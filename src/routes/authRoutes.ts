daa87b6f1dd71ebdf3982288c660d161b55e8493
Taylor Caldwell <tcaldwell@twitter.com>
2025-06-12 16:32:50 -0700

    fix

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import crypto from 'crypto';
 import { TwitterApi } from 'twitter-api-v2';
 import dotenv from 'dotenv';
